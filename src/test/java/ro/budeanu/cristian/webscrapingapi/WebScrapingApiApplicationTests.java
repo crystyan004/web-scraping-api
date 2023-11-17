29455a54f81138e701d6a56b7192b01778dec89f
cbudeanu <cbudeanu@estee.com>
2023-11-17 16:21:04 +0200

    project init

---
+++ src/test/java/ro/budeanu/cristian/webscrapingapi/WebScrapingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.budeanu.cristian.webscrapingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebScrapingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
